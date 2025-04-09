4d85da2e24150b9ccbaa019a50030a31b67429e6
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2025-04-09 17:44:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,14 +98,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/links", linkRoutes);
 app.use("/api/redirect", redirectRoutes);
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+//   });
+// }
 
 // Start server
 const PORT = process.env.PORT || 5000;
